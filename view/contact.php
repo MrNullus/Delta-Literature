baa4264008114c2c28a48c68fd150017e8ed9752
Ms. Nullus <70487766+msNullus@users.noreply.github.com>
2022-08-02 18:36:10 -0300

    add new changes in new structure

---
+++ view/contact.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>ΔLiterature</title>
+
+    <!-- link for files css -->
+    <link rel="stylesheet" href="assets/css/globals.css" />
+    <link rel="stylesheet" href="assets/css/contact.css" />
+  </head>
+
+  <body>
+    <!-- container -->
+    <div class="container">
+
+      <!-- header page -->
+      <header class="header-page">
+        <!-- box logo -->
+        <div class="box-logo">
+          <h1 class="logo">
+            ΔLiterature
+          </h1>
+        </div>
+
+        <!-- navbar -->
+        <nav class="navbar">
+          <!-- nav links -->
+          <ul class="nav-links">
+            <!-- nav link -->
+            <li class="nav-link">
+              <a href="index.html">Home</a>
+            </li>
+            <!-- nav link -->
+            <li class="nav-link">
+              <a href="lord-byron.html">Lord Byron</a>
+            </li>
+            <!-- nav link -->
+            <li class="nav-link">
+              <a href="edgar-alan-poe.html">Edgar Alan Poe</a>
+            </li>
+            <!-- nav link -->
+            <li class="nav-link">
+              <a href="augusto-dos-anjos.html">Augusto dos Anjos</a>
+            </li>
+             <!-- nav link -->
+             <li class="nav-link">
+              <a href="index.html">Home</a>
+            </li>
+          </ul>
+        </nav>
+
+      </header>
+      <!-- /header page -->
+
+      <!-- page -->
+      <main class="page contact">
+        <!-- header -->
+        <header>
+          <h2 class="heading">
+            Entre em contato conosco!📜
+          </h2>
+        </header>
+
+        <!-- container contact -->
+        <section class="container-contact">
+          <!-- form -->
+          <form action="#">
+            <label for="name">Nome</label>
+            <input type="text" id="fname" name="firstname" placeholder="Seu nome...">
+
+            <label for="name">Email</label>
+            <input type="email" id="email" name="email" placeholder="Seu email...">
+        
+            <label for="subject" class="sr-only">Mensagem</label>
+            <textarea id="subject" name="subject" placeholder="O que deseja escrever?" style="height:200px"></textarea>
+        
+            <input type="submit" value="Enviar">
+          </form>
+          <!-- /form -->
+
+        </section>
+        <!-- /container contact -->
+
+      </main>
+      <!-- /page -->
+
+      <!-- footer page -->
+      <footer class="footer-page">
+        <h5 class="footer-logo">
+          ΔLiterature 💜
+        </h5>
+      </footer>
+    </div>
+    <!-- /container -->
+
+  </body>
+
+</html>
